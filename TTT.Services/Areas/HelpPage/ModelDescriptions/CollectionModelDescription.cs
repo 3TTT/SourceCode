a0181cfe191da6e191f072699605da97590b48f2
3TTT <traineetotrainer@gmail.com>
2016-01-24 16:13:58 +0530

    Services and Core project added

---
+++ TTT.Services/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TTT.Services.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TTT.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TTT.Services.WebApiApplication" Language="C#" %>
